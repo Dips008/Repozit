96450fbb3fe317fd1c06c529245b9928fc07f2a1
Igor_Demchenko <idemch777@gmail.com>
2022-10-07 20:10:50 +0300

    Add Ex_008 - Ex_011

---
+++ Ex010_MatodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 99, 35, 13, 65, 4, 6, 7, 99 };
+int n = array.Length;
+int find = 99;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
