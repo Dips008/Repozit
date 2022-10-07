6921985d7213c3f47d2f7d4e1295d62f8380e633
Igor_Demchenko <idemch777@gmail.com>
2022-10-07 22:26:23 +0300

    Add Ex_013

---
+++ Ex_013_maxchifra/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа выводит случайное число из диапазона [ 10, 99] 
+// и находит максимальную цифру в числе
+Console.Clear();
+int num = new Random().Next(10, 100);
+int a1 = num / 10;
+int a2 = num % 10;
+int max = a1;
+if (a2 > max) max = a2;
+Console.WriteLine($"в числе {num} максимальная цифра {max}");
